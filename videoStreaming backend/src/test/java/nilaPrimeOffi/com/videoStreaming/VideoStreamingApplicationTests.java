1e9e21d62245dbd9edef3e719d572a0be92f4de9
nilavanan-ver-4 <nilalinuxa4n@gmail.com>
2024-06-24 07:20:31 +0530

    initialized

---
+++ videoStreaming backend/src/test/java/nilaPrimeOffi/com/videoStreaming/VideoStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package nilaPrimeOffi.com.videoStreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
